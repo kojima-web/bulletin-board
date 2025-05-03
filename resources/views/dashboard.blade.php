819e589f1aaa80f0359c960dfafe211889f41d0f
小島健太郎 <kojimakentarou@King-GnuMacBook-Pro.local>
2025-05-04 04:07:25 +0900

    ダッシュボードに投稿一覧と新規投稿へのリンクを追加

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,12 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
+@section('content')
+    <div class="py-12 px-6">
+        <h1 class="text-2xl font-bold mb-4">ダッシュボード</h1>
+
+        <ul class="list-disc pl-5 space-y-2">
+            <li><a href="{{ route('posts.index') }}" class="text-blue-600 underline">▶ 投稿一覧ページへ</a></li>
+            <li><a href="{{ route('posts.create') }}" class="text-green-600 underline">＋ 新規投稿はこちら</a></li>
+        </ul>
     </div>
-</x-app-layout>
+@endsection
